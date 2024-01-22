23faa7335fbf938617064fc03b2d65e625b6c096
Hyungsoo Park <hyungsoo.park@mail.utoronto.ca>
2024-01-22 04:50:20 +0000

    Download and Save the dataset

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,16 +11,23 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
+library(dplyr)
 # [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# get package
+package <- show_package("4bc5511d-0ecf-487a-9214-7b2359ad8f61")
+package
 
+# get all resources for this package
+resources <- list_package_resources("4bc5511d-0ecf-487a-9214-7b2359ad8f61")
 
+# identify datastore resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the data resource
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+write_csv(data, "inputs/data/unedited_shooting.csv")
